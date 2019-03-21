a0129add84090d9d34e8a026808e613ef910b0d3
zzKeung <keung_997@hotmail.com>
2019-03-21 13:27:12 +0800

    auth1

--- src/utils/routerControl.js
+++ src/utils/routerControl.js
@@ -1,6 +1,7 @@
 import router from "../router/index"
 import vue from "vue"
 import store from '../store/index'
+import {getToken} from './setAuth'
 //主要做些登录拦截
 router.beforeEach((to, from, next) => {
     if(store.state.token){
